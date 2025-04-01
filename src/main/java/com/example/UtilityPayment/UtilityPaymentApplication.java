b8fde3e0771c13e3bdc20b92fb417f6e8dcc1075
Akshaya Manickam <akshayaam49@gmail.com>
2025-04-01 07:06:00 +0530

    Added Spring Boot backend

---
+++ src/main/java/com/example/UtilityPayment/UtilityPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.UtilityPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityPaymentApplication.class, args);
+	}
+
+}
